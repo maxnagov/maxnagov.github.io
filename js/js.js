4a7a9acffb6e63fb89e882109191a0211f7597bb
maxnagov <30313809+maxnagov@users.noreply.github.com>
2017-07-22 16:16:24 +0300

    Add files via upload

---
+++ js/js.js
@@ -0,0 +1,35 @@
+var a = document.getElementById('d');
+document.getElementById('c').addEventListener("mouseover", function(){a.style.opacity ='1';
+this.addEventListener("mouseout", function(){a.style.opacity = '';});
+});
+
+var b = document.getElementById('d1');
+document.getElementById('c1').addEventListener("mouseover", function(){b.style.opacity ='1';
+this.addEventListener("mouseout", function(){b.style.opacity = '';});
+});
+
+var c = document.getElementById('d2');
+document.getElementById('c2').addEventListener("mouseover", function(){c.style.opacity ='1';
+this.addEventListener("mouseout", function(){c.style.opacity = '';});
+});
+
+var d = document.getElementById('d3');
+document.getElementById('c3').addEventListener("mouseover", function(){d.style.opacity ='1';
+this.addEventListener("mouseout", function(){d.style.opacity = '';});
+});
+
+var i = document.getElementById('d4');
+document.getElementById('c4').addEventListener("mouseover", function(){i.style.opacity ='1';
+this.addEventListener("mouseout", function(){i.style.opacity = '';});
+});
+
+var f = document.getElementById('d5');
+document.getElementById('c5').addEventListener("mouseover", function(){f.style.opacity ='1';
+this.addEventListener("mouseout", function(){f.style.opacity = '';});
+});
+
+var g = document.getElementById('d6');
+document.getElementById('c6').addEventListener("mouseover", function(){g.style.opacity ='1';
+this.addEventListener("mouseout", function(){g.style.opacity = '';});
+});
+
